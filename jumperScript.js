f2f7d63e33bdfdcab4a2d415ccfe095bce4dac7d
Aman Raj <amanraj68bittu@gmail.com>
2022-11-21 12:54:08 +0530

    done

--- jumperScript.js
+++ jumperScript.js
@@ -1,9 +1,5 @@
 window.onload = function() {
-    let btn = document.getElementById('jump');
     let count = 0;
-    btn.onclick = function() {
-        count++;
-    }
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     var x = 300;
@@ -12,4 +8,48 @@ window.onload = function() {
     context.arc(x, y, 50, 0, 2 * Math.PI);
     context.fillStyle = "red";
     context.fill();
+
+    context.font = '25px Arial';
+    context.fillStyle = 'white';
+    context.fillText('Count: ' + count, 20, 20);
+
+    document.onkeydown = function() {
+        count += 1;
+        draw();
+        y -= 25;
+    }
+    document.ontouchstart = function() {
+        count += 1;
+        y -= 25;
+        draw();
+    }
+    let t = Date.now();
+    let speed = 25;
+
+    function draw() {
+
+        let timePassed = (Date.now() - t) / 1000;
+        t = Date.now();
+        context.clearRect(0, 0, 600, 400);
+        context.beginPath();
+        context.arc(x, y, 50, 0, 2 * Math.PI);
+        context.fillStyle = 'red';
+        context.fill();
+
+        context.font = '25px Arial';
+        context.fillStyle = 'white';
+        context.fillText('Count: ' + count, 20, 20);
+
+        if (y < 350) {
+            speed += 50 * timePassed;
+            y += speed * timePassed;
+        }
+        if (y > 350) {
+            count = 0;
+            speed = 25;
+        }
+
+        window.requestAnimationFrame(draw);
+    }
+
 }
